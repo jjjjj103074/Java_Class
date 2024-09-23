097f501dc995fc01dd53d97b74788b0813d11bd5
956zs <n1catgx@gmail.com>
2024-09-23 16:31:48 +0800

    add try catch

--- Day1/Examination_system/Examination_system.java
+++ Day1/Examination_system/Examination_system.java
@@ -23,8 +23,10 @@ public class Examination_system {
     public static void main(String[] args) {
         ArrayList<Student> students = new ArrayList<>();
         String input;
+        int age, score;
 
         while (true) {
+
             System.out.print("請輸入考生的姓名（或輸入 'q' 退出）:");
             input = System.console().readLine();
 
@@ -33,18 +35,31 @@ public class Examination_system {
             }
 
             String name = input;
+            while (true) {
+                System.out.print("請輸入考生的年齡:");
+                try {
+                    age = Integer.parseInt(System.console().readLine()); // ��字元���������字
+                    break;
+                } catch (NumberFormatException e) {
+                    System.out.println("輸入的年齡不是數字。請重新輸入。");
+                }
+            }
+            while (true) {
 
-            System.out.print("請輸入考生的年齡:");
-            int age = Integer.parseInt(System.console().readLine()); // 將字元轉換為數字
-
-            System.out.print("請輸入考生的 Java 考試分數:");
-            int score = Integer.parseInt(System.console().readLine()); // 將字元轉換為數字
+                System.out.print("請輸入考生的 Java 考試分數:");
+                try {
+                    score = Integer.parseInt(System.console().readLine());// 將字元轉換為數字
+                    break;
+                } catch (NumberFormatException e) {
+                    System.out.println("輸入的分數不是數字。請重新輸入。");
+                }
+            }
 
             // 創建考生物件並添加到列表中
             Student student = new Student(name, age, score);
             students.add(student);
-        }
 
+        }
         // 印出所有考生的資料
         System.out.println("\n所有考生的資料:");
         for (int i = 0; i < students.size(); i++) {
